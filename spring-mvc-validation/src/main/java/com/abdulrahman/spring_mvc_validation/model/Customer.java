5c473293a02d4fb5096f3a2b3ff8c04f25ac8a5a
abdalrhman <abdalrhmanmogahed2002@gmail.com>
2025-11-27 23:06:17 +0200

    validate input using regular expression

--- spring-mvc-validation/src/main/java/com/abdulrahman/spring_mvc_validation/model/Customer.java
+++ spring-mvc-validation/src/main/java/com/abdulrahman/spring_mvc_validation/model/Customer.java
@@ -1,9 +1,6 @@
 package com.abdulrahman.spring_mvc_validation.model;
 
-import jakarta.validation.constraints.Max;
-import jakarta.validation.constraints.Min;
-import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Size;
+import jakarta.validation.constraints.*;
 
 public class Customer {
 
@@ -13,9 +10,21 @@ public class Customer {
     @Size(min = 1, message = "is required")
     private String lastName;
 
+    @NotNull(message = "is required")
     @Min(value = 1, message = "Age must be more than of equal one.")
     @Max(value = 120, message = "You lived too much.")
     private Integer age;
+
+    @Pattern(regexp = "^[a-zA-Z0-9]{5}", message = "Postal ode can be only 5 chars/digits.")
+    private String postalCode;
+
+    public @Pattern(regexp = "^[a-zA-Z0-9]{5}", message = "Postal ode can be only 5 chars/digits.") String getPostalCode() {
+        return postalCode;
+    }
+
+    public void setPostalCode(@Pattern(regexp = "^[a-zA-Z0-9]{5}", message = "Postal ode can be only 5 chars/digits.") String postalCode) {
+        this.postalCode = postalCode;
+    }
 
     public @Min(value = 1, message = "Age must be more than of equal one.") @Max(value = 120, message = "You lived too much.") Integer getAge() {
         return age;
