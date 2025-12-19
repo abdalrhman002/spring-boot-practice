87d187307707e61d9b996a92903b310916d05297
abdalrhman <abdalrhmanmogahed2002@gmail.com>
2025-12-19 23:51:08 +0200

    add /list mapping

---
+++ spring-boot-spring-mvc-crud-starter/src/main/java/com/luv2code/springboot/cruddemo/controller/EmployeeController.java
@@ -0,0 +1,39 @@
+package com.luv2code.springboot.cruddemo.controller;
+
+import com.luv2code.springboot.cruddemo.entity.Employee;
+import com.luv2code.springboot.cruddemo.service.EmployeeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/employees")
+public class EmployeeController {
+
+    private EmployeeService employeeService;
+
+    // Inject employee service object to the employee controller
+    @Autowired
+    public EmployeeController(EmployeeService employeeService){
+
+        this.employeeService = employeeService;
+    }
+
+    // add mapping for "/list"
+    @GetMapping("/list")
+    public String listEmployees(Model model){
+
+        // get employees form the database
+        List<Employee> employees = employeeService.findAll();
+
+        // add employees to the model
+        model.addAttribute("employees", employees);
+
+        return "employees-list";
+    }
+
+}
