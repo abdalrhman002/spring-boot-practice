2035e48ef6379e8cd8065c5f0d7f6baa8284c6ac
abdalrhman <abdalrhmanmogahed2002@gmail.com>
2026-05-11 23:13:40 +0300

    dive in CSRF internal flow

--- spring-security-lab/src/main/java/com/am9/spring_security_lab/config/SecurityConfig.java
+++ spring-security-lab/src/main/java/com/am9/spring_security_lab/config/SecurityConfig.java
@@ -1,12 +1,16 @@
 package com.am9.spring_security_lab.config;
 
+import com.am9.spring_security_lab.filter.CsrfCookieFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
@@ -16,10 +20,16 @@ public class SecurityConfig {
 
     @Bean
     SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
+
+        http.sessionManagement(session -> session
+                .sessionCreationPolicy(SessionCreationPolicy.IF_REQUIRED)
+        );
         http.authorizeHttpRequests((requests) -> requests
-                .requestMatchers("/register").permitAll()
+//                .requestMatchers("/register").permitAll()
                 .anyRequest().authenticated());
-        http.csrf(csrfConfig -> csrfConfig.disable());
+        http.csrf(csrfConfig -> csrfConfig
+                .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse()));
+        http.addFilterAfter(new CsrfCookieFilter(), BasicAuthenticationFilter.class);
         http.formLogin(withDefaults());
         http.httpBasic(withDefaults());
         return http.build();
